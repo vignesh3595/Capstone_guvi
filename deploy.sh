6776a60e98c753ca89f57345c2b007cc14c0130d
vignesh3595 <152413995+vignesh3595@users.noreply.github.com>
2024-03-18 14:48:45 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-docker run -itd -p 80:80 capstone
+# docker run -itd -p 80:80 capstone
 docker login -u vigneshvkr -p $docker_pass
-if["$GIT_BRANCH" == "origin/dev"]; then
-docker tag capstone vigneshvkr/dev:v1 .
+if [["$GIT_BRANCH" == "origin/dev"]]; then
+docker tag capstone vigneshvkr/dev:v1
 docker push vigneshvkr/dev:v1
-elif["$GIT_BRANCH" == "origin/main"]; then
-docker tag capstone vigneshvkr/prod:v2 .
+elif [["$GIT_BRANCH" == "origin/main"]]; then
+docker tag capstone vigneshvkr/prod:v2
 docker push vigneshvkr/prod:v2
 else
 echo "No action. Branch is not dev or main."
