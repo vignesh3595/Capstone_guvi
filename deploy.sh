df58a5e1d781ce3d80260db95a35a2e26266368f
vignesh3595 <152413995+vignesh3595@users.noreply.github.com>
2024-03-18 14:39:26 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 docker run -itd -p 80:80 capstone
 docker login -u vigneshvkr -p $docker_pass
-if["$GIT_BRANCH" == "origin/dev"];
+if["$GIT_BRANCH" -eq "origin/dev"]; 
 then
 docker tag capstone vigneshvkr/dev:v1 .
 docker push vigneshvkr/dev:v1
-elif["$GIT_BRANCH" == "origin/main"];
+elif["$GIT_BRANCH" -eq "origin/main"];
 then
 docker tag capstone vigneshvkr/prod:v2 .
 docker push vigneshvkr/prod:v2
